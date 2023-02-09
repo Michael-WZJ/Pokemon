7cc3c0b637a780fc35bcb4fc256ba6938842a7d7
Michael <a379605996z@gmail.com>
2023-02-09 19:27:12 +0800

    Init Project

---
+++ MyPokemon/src/main/java/com/michaelj/MyPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.michaelj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPokemonApplication.class, args);
+	}
+
+}
