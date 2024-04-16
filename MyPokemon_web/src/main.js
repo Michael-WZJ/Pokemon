6de8357d66e82fb664da4718076b73e51410fdad
万智俊 <wanzhijun2020@163.com>
2024-04-16 11:32:52 +0800

    feat: 【前端 Vue】 添加依赖 axios、element-ui、avue

--- MyPokemon_web/src/main.js
+++ MyPokemon_web/src/main.js
@@ -2,7 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 
+// 使用Avue需要先安装ElementUI的依赖
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import Avue from '@smallwei/avue';
+import '@smallwei/avue/lib/index.css';
+
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+Vue.use(Avue);
 
 new Vue({
   render: h => h(App),
