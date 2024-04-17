f108e416ce0e68538e1d66499c518a02db14c715
万智俊 <wanzhijun2020@163.com>
2024-04-17 18:47:45 +0800

    feat: 异常处理器 处理 DTO校验异常

--- MyPokemon/src/main/java/com/michaelj/infrastructure/exception/ProjectExceptionAdvice.java
+++ MyPokemon/src/main/java/com/michaelj/infrastructure/exception/ProjectExceptionAdvice.java
@@ -2,15 +2,37 @@ package com.michaelj.infrastructure.exception;
 
 import com.michaelj.domain.Code;
 import com.michaelj.domain.base.Result;
+import com.michaelj.infrastructure.constant.BaseConst;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 //TODO 异常处理器
 @RestControllerAdvice
 public class ProjectExceptionAdvice {
+    private static final String SPLIT= "; ";
+
     @ExceptionHandler(BusinessException.class)
     public Result doBusinessException(BusinessException ex) {
         return new Result(ex.getCode(), null, ex.getMessage());
+    }
+
+    /**
+     * 处理 DTO Validated 校验异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Result handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+        List<String> errMsgList = errors.stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
+        String errMsg = errors.stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(SPLIT));
+        return new Result(Code.PROJECT_VALIDATE_ERR.getCode(), errMsgList, errMsgList.get(BaseConst.FIRST_ITEM));
     }
 
     /**
