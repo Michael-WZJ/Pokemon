5b77e6b7b4dfee9477162a6c5e3918dd4c7e1085
万智俊 <wanzhijun2020@163.com>
2026-01-05 17:32:35 +0800

    feat: 【后端】 [配置管理] 新增、编辑、删除

--- MyPokemon/src/main/java/com/michaelj/infrastructure/constant/BaseConst.java
+++ MyPokemon/src/main/java/com/michaelj/infrastructure/constant/BaseConst.java
@@ -4,6 +4,8 @@ public class BaseConst {
     public static final int FIRST_ITEM = 0;
     public static final String SPLIT_SEMICOLON = ";";
     public static final String SPLIT_CAESURA = "、";
+    public static final String SPLIT_DOT = ".";
+
     public static final String BLANK = "";
     public static final String FIRST_CODE = "0001";
 }
--- MyPokemon/src/main/java/com/michaelj/infrastructure/constant/PokeExceptionEnum.java
+++ MyPokemon/src/main/java/com/michaelj/infrastructure/constant/PokeExceptionEnum.java
@@ -2,13 +2,28 @@ package com.michaelj.infrastructure.constant;
 
 import com.michaelj.domain.Code;
 
-public enum PokeExceptionEnum implements BaseExceptionEnum{
+public enum PokeExceptionEnum implements BaseExceptionEnum {
+    COMMON_100001(Code.PROJECT_VALIDATE_ERR, "id不能为空！"),
+    COMMON_100002(Code.SAVE_ERR, "新增 存在重复数据！"),
+    COMMON_100003(Code.GET_ERR, "未查询到数据！"),
+    COMMON_100004(Code.GET_ERR, "查询到多条数据！"),
+
+
     CODE_REPEAT_FAIL(Code.PROJECT_VALIDATE_ERR, "宝可梦编号重复"),
     NAME_REPEAT_FAIL(Code.PROJECT_VALIDATE_ERR, "宝可梦名称重复"),
     EVOL_REPEAT_FAIL(Code.PROJECT_VALIDATE_ERR, "已有该进化"),
     EVOL_NOT_FOUND_FAIL(Code.PROJECT_VALIDATE_ERR, "该进化链不存在"),
     EVOL_PARAM_FAIL(Code.PROJECT_VALIDATE_ERR, "进化链参数不能为空"),
     EVOL_PATERNAL_NOT_FOUND_FAIL(Code.PROJECT_VALIDATE_ERR, "未找到进化型: {0}"),
+
+
+    CHILDERN_EXIST_ERR(Code.DELETE_ERR, "存在子配置！"),
+
+
+    FILE_PATH_BLANK_ERR(Code.PROJECT_BUSINESS_ERR, "文件路径不能为空"),
+    RESOURCE_ATTACH_ERR(Code.PROJECT_BUSINESS_ERR, "读取资源异常"),
+
+
     EXCEL_PARSE_FAIL(Code.PROJECT_BUSINESS_ERR, "Excel文件解析错误"),
     EXCEL_EXPORT_FAIL(Code.PROJECT_BUSINESS_ERR, "导出失败"),
     ;
