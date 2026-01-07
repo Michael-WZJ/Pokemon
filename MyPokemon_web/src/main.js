4535ac9b93ba0d6d168b08d966e3dc07b2686646
万智俊 <wanzhijun2020@163.com>
2026-01-07 11:50:26 +0800

    feat: 【前端 Vue】 [宝可梦基本信息] 添加 全局css

--- MyPokemon_web/src/main.js
+++ MyPokemon_web/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router/index.js'
 import MjSelect from "@/components/mj-select/index.vue";
 
+import '@/assets/css/global.css'
+
 // 使用Avue需要先安装ElementUI的依赖
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
